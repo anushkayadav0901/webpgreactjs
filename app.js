a77a13829073dd6452ddb3a2998c042226070e9a
anushkayadav0901 <anushkayadav09012007@gmail.com>
2024-09-02 17:48:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ footer.js
@@ -0,0 +1,12 @@
+// src/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 My Webpage</p>
+    </footer>
+  );
+}
+
+export default Footer;
