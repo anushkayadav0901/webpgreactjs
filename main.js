fb824f6f57111f61e45fc72f6987b72c4aec8637
anushkayadav0901 <anushkayadav09012007@gmail.com>
2024-09-07 17:10:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// src/Main.js
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      <p>This is the main content area of my webpage.</p>
+    </main>
+  );
+}
+
+export default Main;
